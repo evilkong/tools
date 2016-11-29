86d52752605307a5dbfc3c3c838e129cb631dff0
u0mo5 <u0mo5@qq.com>
2016-11-29 10:04:06 +0800

    Arrays Strings Times add        import    from   thinkphp5   topthink  framework

--- src/Arrays.php
+++ src/Arrays.php
@@ -66,6 +66,31 @@ class Arrays {
         return $out;
     }
 
+//from topthink
+    public static function isAssoc(array $array)
+    {
+        $keys = array_keys($array);
+
+        return array_keys($keys) !== $keys;
+    }
+
+    public static function sortRecursive($array)
+    {
+        foreach ($array as &$value) {
+            if (is_array($value)) {
+                $value = static::sortRecursive($value);
+            }
+        }
+
+        if (static::isAssoc($array)) {
+            ksort($array);
+        } else {
+            sort($array);
+        }
+
+        return $array;
+    }
+
 
 
 }
--- src/Strings.php
+++ src/Strings.php
@@ -94,8 +94,6 @@ echo cut_str($str, 1, 0).'**'.cut_str($str, 1, -1);
     /**
      * 查询字符是否存在于某字符串
      *
-     * @param $haystack 字符串
-     * @param $needle 要查找的字符
      * @return bool
      */
     public static function str_exists($haystack, $needle)
@@ -115,6 +113,119 @@ echo cut_str($str, 1, 0).'**'.cut_str($str, 1, -1);
         }
         return $str;
     }
+//from topthink
+    /**
+     * 检查字符串中是否包含某些字符串
+     * @param string       $haystack
+     * @param string|array $needles
+     * @return bool
+     */
+    public static function contains($haystack, $needles)
+    {
+        foreach ((array)$needles as $needle) {
+            if ($needle != '' && mb_strpos($haystack, $needle) !== false) {
+                return true;
+            }
+        }
 
+        return false;
+    }
+
+    /**
+     * 检查字符串是否以某些字符串结尾
+     *
+     * @param  string       $haystack
+     * @param  string|array $needles
+     * @return bool
+     */
+    public static function endsWith($haystack, $needles)
+    {
+        foreach ((array)$needles as $needle) {
+            if ((string)$needle === static::substr($haystack, -static::length($needle))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 检查字符串是否以某些字符串开头
+     *
+     * @param  string       $haystack
+     * @param  string|array $needles
+     * @return bool
+     */
+    public static function startsWith($haystack, $needles)
+    {
+        foreach ((array)$needles as $needle) {
+            if ($needle != '' && mb_strpos($haystack, $needle) === 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * 获取指定长度的随机字母数字组合的字符串
+     *
+     * @param  int $length
+     * @return string
+     */
+    public static function randomSN($length = 16)
+    {
+        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        return static::substr(str_shuffle(str_repeat($pool, $length)), 0, $length);
+    }
+
+
+    /**
+     * 字符串转小写
+     *
+     * @param  string $value
+     * @return string
+     */
+    public static function lower($value)
+    {
+        return mb_strtolower($value, 'UTF-8');
+    }
+
+    /**
+     * 字符串转大写
+     *
+     * @param  string $value
+     * @return string
+     */
+    public static function upper($value)
+    {
+        return mb_strtoupper($value, 'UTF-8');
+    }
+
+    /**
+     * 获取字符串的长度
+     *
+     * @param  string $value
+     * @return int
+     */
+    public static function length($value)
+    {
+        return mb_strlen($value);
+    }
+
+    /**
+     * 截取字符串
+     *
+     * @param  string   $string
+     * @param  int      $start
+     * @param  int|null $length
+     * @return string
+     */
+    public static function substr($string, $start, $length = null)
+    {
+        return mb_substr($string, $start, $length, 'UTF-8');
+    }
 
 }
