9ef04a2123feeca4fa0b51f5a25ef1d69b72f71a
u0mo5 <u0mo5@qq.com>
2017-04-14 17:26:55 +0800

    Dbs

---
+++ src/Apis/Ip.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by u0mo5.
+ * User: u0mo5
+ * Date: 2015/9/30
+ * Time: 13:36
+ * T\Apis\Qrcode::show('http://bookfuns.com');
+ */
+namespace U0mo5\Tools\Apis;
+class Ip{
+
+//sina 接口
+    public static function sina($ip){
+
+        $url="http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip=".$ip;
+        $data=file_get_contents($url);
+        return $json=json_decode($data);
+    }
+//默认url
+    public static function callback($url=""){
+        $data=file_get_contents($url);
+        return json_decode($data);
+    }
+//直接显示
+    public static function show($ip,$callback="callback"){
+    $url=self::$callback($ip);
+    var_dump( $url);
+
+}
+    public static function get($ip,$callback="callback"){
+        $url=self::$callback($ip);
+        return ( $url);
+
+    }
+
+
+
+}
