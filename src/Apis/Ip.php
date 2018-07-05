6846e0c50415ebb0346299629aa1e6de1f921de9
u0mo5@qq.com <u0mo5@qq.com>
2018-07-05 14:50:26 +0800

    ip

--- src/Apis/Ip.php
+++ src/Apis/Ip.php
@@ -4,42 +4,50 @@
  * User: u0mo5
  * Date: 2015/9/30
  * Time: 13:36
- * T\Apis\Qrcode::show('http://bookfuns.com');
+ * T\Apis\Qrcode::show('http://bookfuns.com');.
  */
+
 namespace U0mo5\Tools\Apis;
-class Ip{
 
-//sina 接口
-    public static function sina($ip){
-
-        $url="http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip=".$ip;
-        $data=file_get_contents($url);
-        return $json=json_decode($data);
-    }
+class Ip
+{
     //sina 接口
-    public static function taobao($ip){
+    public static function sina($ip)
+    {
+        $url = 'http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip='.$ip;
+        $data = file_get_contents($url);
 
-        $url="http://ip.taobao.com/service/getIpInfo.php?ip=".$ip;
-        $data=file_get_contents($url);
-        return $json=json_decode($data);
+        return $json = json_decode($data);
     }
-//默认url
-    public static function callback($url=""){
-        $data=file_get_contents($url);
+
+    //sina 接口
+    public static function taobao($ip)
+    {
+        $url = 'http://ip.taobao.com/service/getIpInfo.php?ip='.$ip;
+        $data = file_get_contents($url);
+
+        return $json = json_decode($data);
+    }
+
+    //默认url
+    public static function callback($url = '')
+    {
+        $data = file_get_contents($url);
+
         return json_decode($data);
     }
-//直接显示
-    public static function show($ip,$callback="callback"){
-    $url=self::$callback($ip);
-    var_dump( $url);
 
-}
-    public static function get($ip,$callback="callback"){
-        $url=self::$callback($ip);
-        return ( $url);
-
+    //直接显示
+    public static function show($ip, $callback = 'callback')
+    {
+        $url = self::$callback($ip);
+        var_dump($url);
     }
 
+    public static function get($ip, $callback = 'callback')
+    {
+        $url = self::$callback($ip);
 
-
+        return  $url;
+    }
 }
